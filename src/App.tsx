ccdf5451c8e47e1ab2d7c6d70c83960de52061bb
moonholiday <727moonholiday@gmail.com>
2023-08-07 16:53:31 +0545

    feat: max limit 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import Root from "./pages";
+import React, { useState } from "react";
+import { Container, Flex } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { getImages } from "./api";
+import Search from "./components/Search";
+import View from "./components/View";
 
-function App() {
-  return <Root />;
-}
+const App: React.FC = () => {
+  // State to hold the query data
+  const [query, setQuery] = useState<string>("");
+
+  const { data: images, isLoading } = useQuery({
+    queryKey: ["images", query],
+    queryFn: () => getImages(query),
+  });
+
+  return (
+    <Container p="4rem" maxW="container.xl">
+      <Flex direction="column" gap="2rem">
+        <Search query={query} setQuery={setQuery} />
+        <View images={images?.results} isLoading={isLoading} />
+      </Flex>
+    </Container>
+  );
+};
 
 export default App;
--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const client_id = "G7alFg4s_Rx4pY1FFPsje98B40ZYtA0zYQ79zmbQlO8";
+const per_page = 9;
 
 export async function getImages(query: string) {
   return axios({
     url: "https://api.unsplash.com/search/photos",
-    params: { query, client_id },
+    params: { query, per_page, client_id },
   }).then((res) => res?.data);
 }
